2bc1022b8cf9ed99161d60b112c47989a66b3b00
swithek <simon.public@pm.me>
2019-09-03 16:32:07 +0300

    improved i3 auto assign script

--- .config/i3/scripts/assign.sh
+++ .config/i3/scripts/assign.sh
@@ -8,7 +8,7 @@ fi
 
 info=($(xprop -id $(xprop -root -f _NET_ACTIVE_WINDOW 0x " \$0\\n" _NET_ACTIVE_WINDOW | \
 	awk "{print \$2}") | \
-	sed -nE 's/^WM_CLASS.*= "(\w+)", "(\w+)"/\1\ \2/p'))
+	sed -nE 's/^WM_CLASS.*= "([_a-zA-Z0-9-]+)", "([_a-zA-Z0-9-]+)"/\1\ \2/p'))
 
 sed -i -E "/^assign.+class=\"${info[1]}\" instance=\"${info[0]}\"/ d;
 	0,/^assign/ s/^(assign)/assign\ [class=\"${info[1]}\" instance=\"${info[0]}\"] \$ws$nr\n\1/"\
