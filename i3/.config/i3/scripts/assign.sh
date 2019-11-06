2c82e58124a2e0b8adadcd452396d6d85723607a
swithek <simon.public@pm.me>
2019-11-06 11:34:53 +0000

    assign script symlinks following

--- i3/.config/i3/scripts/assign.sh
+++ i3/.config/i3/scripts/assign.sh
@@ -10,6 +10,6 @@ info=($(xprop -id $(xprop -root -f _NET_ACTIVE_WINDOW 0x " \$0\\n" _NET_ACTIVE_W
 	awk "{print \$2}") | \
 	sed -nE 's/^WM_CLASS.*= "([_a-zA-Z0-9-]+)", "([_a-zA-Z0-9-]+)"/\1\ \2/p'))
 
-sed -i -E "/^assign.+class=\"${info[1]}\" instance=\"${info[0]}\"/ d;
+sed --follow-symlinks -i -E "/^assign.+class=\"${info[1]}\" instance=\"${info[0]}\"/ d;
 	0,/^assign/ s/^(assign)/assign\ [class=\"${info[1]}\" instance=\"${info[0]}\"] \$ws$nr\n\1/"\
 		$HOME/.config/i3/config
